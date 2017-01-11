f01ba644776cb784361803d5a2eaaa7d1ab36bfd
xenma <poma@kb.dk>
2017-01-11 15:24:30 +0100

    Move most of the variables in a gulp.config file

---
+++ gulp.config.js
@@ -0,0 +1,31 @@
+// File to keep our configuration settings
+
+module.exports = function() {
+    var config = {
+
+        // the js source code
+        JSSRC: 'http-pub/js/*.js',
+        // all the js to validate
+        alljs: ['http-pub/js/*.js', './*.js'],
+        //external libraries
+        externalJSSRC: ['http-pub/3rdparty/openseadragon.js', 'http-pub/3rdparty/native.history.js'],
+        // the CSS files
+        CSSSRC: 'http-pub/css/*.css',
+        // the images
+        IMGSRC: 'http-pub/images/*',
+        // HTML source files
+        HTMLSRC: 'http-pub/*.html',
+        // name of the production destination folder
+        DEST: 'production',
+        // deveopment folder
+        DEVDEST: 'development',
+        // dist folder
+        DISTDEST: 'dist',
+        // localhost URL
+        LOCALHOSTURL: 'http://localhost:8002/',
+        // local IP address
+        IP_LOCALHOSTURL: 'http://10.6.1.83:8001/' // TO BE CHANGED BY DEVELOPERS
+
+    };
+    return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 /* global require, console, process */
 var gulp = require('gulp');
+var args = require('yargs').argv;
+var del = require('del');
+var argv = require('optimist').argv;
+var config = require('./gulp.config')(); // require and run the local config file/function
+
+// gulp plugins
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
@@ -12,20 +18,7 @@ var gzip = require('gulp-gzip');
 var jshint = require('gulp-jshint');
 var gulpprint = require('gulp-print');
 var gulpif = require('gulp-if');
-var args = require('yargs').argv;
 
-var del = require('del');
-var argv = require('optimist').argv;
-
-var externalJSSRC = ['http-pub/3rdparty/openseadragon.js', 'http-pub/3rdparty/native.history.js'];
-var JSSRC = 'http-pub/js/*.js';
-var CSSSRC = 'http-pub/css/*.css';
-var IMGSRC = 'http-pub/images/*';
-var HTMLSRC = 'http-pub/*.html';
-var DEST = 'production';
-var DEVDEST = 'development';
-var DISTDEST = 'dist';
-var LOCALHOSTURL = 'http://localhost:8002/';
 var STATICURL = argv.dest || 'https://static.kb.dk/kbOpenSeadragon/';
 
 if (STATICURL.charAt(STATICURL.length - 1) !== '/') {
@@ -49,7 +42,7 @@ gulp.task('default', ['production'], function () {});
 
 gulp.task('validate', function(){
    return gulp
-       .src([JSSRC, './*.js']) // we want to check both the js files and the gulpfile.js in the root
+       .src(config.alljs) // we want to check both the js files and the gulpfile.js in the root
        .pipe(gulpif(args.verbose, gulpprint())) // print the files that are being validated if one uses --verbose
        .pipe(jshint())
        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
@@ -57,7 +50,7 @@ gulp.task('validate', function(){
 });
 
 gulp.task('clean', function (cb) {
-    del([DEST, DEVDEST, DISTDEST]).then(function (paths) {
+    del([config.DEST, config.DEVDEST, config.DISTDEST]).then(function (paths) {
         gutil.log('deleted paths:', paths);
         if ('undefined' !== typeof cb) {
             cb();
@@ -69,32 +62,32 @@ gulp.task('development', ['clean'], function (cb) {
     gutil.log('Building a ', gutil.colors.cyan('development'), 'build for', gutil.colors.green('"' + STATICURL + '"'));
     // move html files
     gutil.log('Moving html...');
-    gulp.src(HTMLSRC)
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEVDEST));
+    gulp.src(config.HTMLSRC)
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEVDEST));
 
     // bundle, minify and move js files
     gutil.log('Moving js ...');
-    gulp.src(JSSRC)
-    .pipe(replace(LOCALHOSTURL, STATICURL))
+    gulp.src(config.JSSRC)
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
     .pipe(concat('KbOSD_bundle.js'))
-    .pipe(gulp.dest(DEVDEST));
+    .pipe(gulp.dest(config.DEVDEST));
 
     // move 3rdpartyJS
-    gulp.src(externalJSSRC)
+    gulp.src(config.externalJSSRC)
     .pipe(chmod(664))
-    .pipe(gulp.dest(DEST + '/3rdparty'));
+    .pipe(gulp.dest(config.DEST + '/3rdparty'));
 
     // minify and move css
     gutil.log('Moving css ...');
-    gulp.src(CSSSRC)
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEVDEST));
+    gulp.src(config.CSSSRC)
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEVDEST));
 
     // moving images
     gutil.log('Moving images ...');
-    gulp.src(IMGSRC)
-    .pipe(gulp.dest(DEST + '/images'));
+    gulp.src(config.IMGSRC)
+    .pipe(gulp.dest(config.DEST + '/images'));
 
     if ('undefined' !== typeof cb) {
         cb();
@@ -105,55 +98,55 @@ gulp.task('production', ['clean'], function (cb) {
     gutil.log('Building a ', gutil.colors.cyan('production'), 'build for', gutil.colors.green('"' + STATICURL + '"'));
     // move html files
     gutil.log('Moving html...');
-    gulp.src(HTMLSRC)
-    .pipe(replace(LOCALHOSTURL, STATICURL))
+    gulp.src(config.HTMLSRC)
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
     .pipe(replace('KbOSD.js','KbOSD_bundle_min.js')) // FIXME: This ought to be more generic, but gulp-replace does not work with regExp on streams??
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest(config.DEST));
 
     // bundling a non minified version and move js files
     gutil.log('Bundling and moving js ...');
-    gulp.src(JSSRC)
+    gulp.src(config.JSSRC)
     .pipe(concat('KbOSD_bundle.js'))
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST + '/js'));
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEST + '/js'));
 
     // minify and move js files
     gutil.log('Minifying and moving js ...');
-    gulp.src(JSSRC)
+    gulp.src(config.JSSRC)
     .pipe(uglify())
     .pipe(concat('KbOSD_bundle.js'))
     .pipe(rename(function (path) {
         path.basename += '_min';
     }))
     .pipe(replace('kbOSD.css','kbOSD_min.css')) // FIXME: This ought to be more generic, but gulp-replace does not work with regExp on streams??
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST + '/js'));
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEST + '/js'));
 
     // move 3rdpartyJS
-    gulp.src(externalJSSRC)
+    gulp.src(config.externalJSSRC)
     .pipe(chmod(664))
-    .pipe(gulp.dest(DEST + '/3rdparty'));
+    .pipe(gulp.dest(config.DEST + '/3rdparty'));
 
     // moving unminified version of css
     gutil.log('Moving non minified version of css ...');
-    gulp.src(CSSSRC)
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST + '/css'));
+    gulp.src(config.CSSSRC)
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEST + '/css'));
 
     // minify and move css
     gutil.log('Minifying and moving css ...');
-    gulp.src(CSSSRC)
+    gulp.src(config.CSSSRC)
     .pipe(cssmin())
     .pipe(rename(function (path) {
         path.basename += '_min';
     }))
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST + '/css'));
+    .pipe(replace(config.LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(config.DEST + '/css'));
 
     // moving images
     gutil.log('Moving images ...');
-    gulp.src(IMGSRC)
-    .pipe(gulp.dest(DEST + '/images'));
+    gulp.src(config.IMGSRC)
+    .pipe(gulp.dest(config.DEST + '/images'));
 
     gutil.log('Production files done. Use', gutil.colors.green('gulp dist'), 'to create a tarball for distribution.');
 
@@ -165,10 +158,10 @@ gulp.task('production', ['clean'], function (cb) {
 gulp.task('dist', function (cb) {
     // tar/zipping distribution file
     gutil.log('Creating a distribution tarball ...');
-    gulp.src(DEST + '/**/*')
+    gulp.src(config.DEST + '/**/*')
     .pipe(tar('kbOpenSeadragon.tar'))
     .pipe(gzip())
-    .pipe(gulp.dest(DISTDEST));
+    .pipe(gulp.dest(config.DISTDEST));
 
     if ('undefined' !== typeof cb) {
         cb();
